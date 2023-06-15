4a02adf25f93d974a3a35f9c7a3e07a7581f82b0
Виктор <viktor123@qwerty.ru>
2023-06-15 23:11:03 +0300

    Выполнил ДЗ № 2

---
+++ Знакомство с C#/ДЗ № 2/Задача № 2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+int a;
+while (true)
+{
+Console.WriteLine("Введите целое число");
+if (int.TryParse(Console.ReadLine(), out a))
+    break;
+Console.WriteLine("Ошибка ввода!");
+}
+int b = a;
+
+while (a >=1000) a = a/10;
+if (a >= 100) Console.WriteLine($"{b} -> {a % 10}");
+else Console.WriteLine($"{b} -> Третьей цифры нет");
---
+++ Знакомство с C#/ДЗ № 2/Задача № 3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+int a;
+while (true)
+{
+Console.WriteLine("Введите целое число от 1 до 7");
+if (int.TryParse(Console.ReadLine(), out a) && (0 < a ) && (a < 8))
+    break;
+Console.WriteLine("Ошибка ввода!");
+}
+if (a < 6) Console.WriteLine($"{a} -> нет");
+else Console.WriteLine($"{a} -> да");
