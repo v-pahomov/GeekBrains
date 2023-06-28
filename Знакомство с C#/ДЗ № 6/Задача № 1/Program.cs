3bfa24e704a61d9dce9e8dd91052bafc66d8a108
Виктор <viktor123@qwerty.ru>
2023-06-28 22:52:31 +0300

    Исправил Задачу № 1 ДЗ № 6

--- Знакомство с C#/ДЗ № 6/Задача № 1/Program.cs
+++ Знакомство с C#/ДЗ № 6/Задача № 1/Program.cs
@@ -2,45 +2,49 @@
 Console.Clear();
 
 int m = GetNumberFromUser("Введите число М", "Ошибка ввода!");
-float[] array = GetArrayFromUser(m);
+float[] array = GetArrayFromUser(m, "Ошибка ввода!");
 int positiv = GetCountPositiv(array);
-Console.WriteLine($"[{String.Join(", ", array)}] -> {positiv}");
-
-
+Console.WriteLine($"[{String.Join("; ", array)}] -> {positiv}");
 
 int GetCountPositiv(float[] arr)
 {
+    int p = 0;
     int count = 0;
     while (count < arr.Length)
     {
         if (arr[count] > 0)
         {
-            count++;
+            p++;
         }
+        count++;
     }
-    return count;
+    return p;
 }
 
-float[] GetArrayFromUser (int size)
+float[] GetArrayFromUser (int size, string errorMessage1)
 {
     int count1 = 0;
     float[] arr1 = new float [size];
     while (count1 < arr1.Length)
     {
         Console.WriteLine ($"Введите число № {count1 + 1}");
-        arr1[count1] = float.Parse(Console.ReadLine() ?? "");
-        count1++;
+        if (float.TryParse(Console.ReadLine(), out arr1[count1]))
+        {
+            count1++;
+        }
+        else Console.WriteLine(errorMessage1);
     }
     return arr1;
 }
 
 int GetNumberFromUser(string message, string errorMessage)
 {
+    int c;
     while (true)
     {
     Console.WriteLine(message);
-    if (int.TryParse(Console.ReadLine(), out m))
-        return m;
+    if (int.TryParse(Console.ReadLine(), out c) && c > 0)
+        return c;
     Console.WriteLine(errorMessage);
     }
 }
