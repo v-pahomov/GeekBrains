7f8d8baea0c7a4ac24f2f999929553ca38a4625e
Виктор <viktor@MacBook-Pro-Viktor.local>
2023-12-05 18:28:48 +0300

    FamilyTree

---
+++ Знакомство с Java/FamilyTree/Human.java
@@ -0,0 +1,2 @@
+package FamilyTree;public class Human {
+}
---
+++ Знакомство с Java/FamilyTree/Main.java
@@ -0,0 +1,25 @@
+package FamilyTree;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        FamilyTree tree = testTree();
+        System.out.println(tree);
+    }
+    static FamilyTree testTree(){
+        FamilyTree tree = new FamilyTree();
+
+        Human vasya = new Human("Василий", Gender.Male, LocalDate.of(1963, 12, 10));
+        Human masha = new Human("Мария", Gender.Female, LocalDate.of(1965, 9, 15));
+        tree.add(vasya);
+        tree.add(masha);
+        tree.setWedding(vasya, masha);
+
+        Human cristina = new Human("Кристина", Gender.Female, LocalDate.of(1988, 7, 5), vasya, masha);
+        Human semen = new Human("Семен", Gender.Male, LocalDate.of(1991, 1, 25), vasya, masha);
+        tree.add(cristina);
+        tree.add(semen);
+        return tree;
+    }
+}
